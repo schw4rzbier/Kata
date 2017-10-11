b986d1c9aea5cededfb4690b9ccb5a34af91bb10
Jeremy Childers <jeremy.seth.childers@gmail.com>
2017-10-10 21:35:12 -0500

    puzzle of the day

---
+++ Kata/ToSmallest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Codewars
+{
+    public static class ToSmallest
+    {
+        public static long[] Smallest(long n)
+        {
+            var nums = n.ToString().ToList().ConvertAll(c => (int)Char.GetNumericValue(c));
+
+            var rtnVars = new long[] {n, 0, 0};
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = 0; j < nums.Count; j++)
+                {
+                    var testDigits = new List<int>(nums);
+                    var d = nums[i];
+                    testDigits.RemoveAt(i);
+                    testDigits.Insert(j, d);
+                    var testSmallest = testDigits.DigitListToLong();
+                    if (testSmallest < rtnVars[0])
+                    {
+                        rtnVars[0] = testSmallest;
+                        rtnVars[1] = i;
+                        rtnVars[2] = j;
+                    }
+                }
+            }
+
+            return rtnVars;
+        }
+
+        public static long DigitListToLong(this List<int> digitList)
+        {
+            long rtnVal = 0;
+
+            for (int i = digitList.Count; i > 0; i--)
+            {
+                rtnVal += digitList[digitList.Count - i] * (long)Math.Pow(10, i-1);
+            }
+            return rtnVal;
+        }
+
+    }
+}
